3385c4014810a10f79cbc50b0380cb84318e7af2
lukeryannetnz <github@lukeryan.net.nz>
2015-05-24 06:35:19 +0000

    Adding model for on/off switch

--- alfredapp/models.py
+++ alfredapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import RPi.GPIO as GPIO
 
-# Create your models here.
+class OnOffSwitchAdapter(models.Model):
+	location = models.CharField(max_length=200)
+	gpioPinBcmIndex = models.IntegerField(default=4)
+	dateAddedUtc = models.DateTimeField('Date added')
+
+	def toggle():
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+
+		GPIO.setup(4, GPIO.OUT)
+		GPIO.output(4, 1)
