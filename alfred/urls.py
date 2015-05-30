01f17b49ad3f48126cae6ae8636484aaa562796c
Luke Ryan <hello@lukeryan.net.nz>
2015-05-30 20:16:42 +1200

    Api behind api route

--- alfred/urls.py
+++ alfred/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('alfredapp.urls')),
+    url(r'^api/', include('alfredapp.urls')),
 ]
