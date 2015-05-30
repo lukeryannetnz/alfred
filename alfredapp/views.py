5f9ca900b557369b544a0128f23de0e7807e858d
Luke Ryan <hello@lukeryan.net.nz>
2015-05-30 20:07:19 +1200

    Added http responsebadrequest

--- alfredapp/views.py
+++ alfredapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.http import JsonResponse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
