4ff4b6be6080152e21d4c0ee7cd6175e3ae7e6f9
Luke Ryan <hello@lukeryan.net.nz>
2015-05-30 20:34:18 +1200

    removed old alfredapp

--- alfredapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-	url(r'^devices/$', views.devices, name='devices'),
-	url(r'^devices/(?P<id>[0-9]+)$', views.devicebyid, name='devicebyid')
-]
