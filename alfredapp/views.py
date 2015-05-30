f2453adc522978b71ac0dbc5f71ddeb6db86a895
Luke Ryan <hello@lukeryan.net.nz>
2015-05-30 19:39:09 +1200

    remember the import

--- alfredapp/views.py
+++ alfredapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import OnOffSwitch
 
