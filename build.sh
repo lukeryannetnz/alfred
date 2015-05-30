1cadb6f26d9c3ab4d98ef64c4bdf55fbe9d4d8e1
Luke Ryan <hello@lukeryan.net.nz>
2015-05-31 10:09:06 +1200

    Adding to setup and build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pylint ./alfredapi/*
 
+python manage.py compilemessages
+
+pylint ./alfredapi/*
--- setup.sh
+++ setup.sh
@@ -3,4 +3,10 @@
 echo "Installing pylink..."
 pip install pylint
 
+echo "Installing gunicorn..."
+pip install gunicorn
+
+echo "Installing raspberry pi RPi.GPIO library..."
+pip install RPi.GPIO
+
 echo "Setup done"
