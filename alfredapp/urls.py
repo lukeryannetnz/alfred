c57d31bbdd21d98d116fdf5d8bebfef7d9ba93a5
lukeryannetnz <github@lukeryan.net.nz>
2015-05-30 06:47:10 +0000

    updating url patterns for getting a device by id

--- alfredapp/urls.py
+++ alfredapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.devices, name='devices'),
+	url(r'^.*devices$', views.devices, name='devices'),
+	url(r'.*devices/.*^$', views.devicebyid, name='devicebyid')
 ]
 
