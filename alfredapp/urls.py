6491ab88559f1e2d47a4af645600d4c130fcd8b8
Luke Ryan <hello@lukeryan.net.nz>
2015-05-30 19:11:24 +1200

    Fixing urls. Now /device/

--- alfredapp/urls.py
+++ alfredapp/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^.*devices$', views.devices, name='devices'),
-	url(r'^.*devices/(?P<id>[0-9]+)$', views.devicebyid, name='devicebyid')
+	url(r'^/$', views.devices, name='devices'),
+	url(r'^/(?P<id>[0-9]+)$', views.devicebyid, name='devicebyid')
 ]
