e8fe1cd4fd7b13c16f2ca08385a4b66b15ca6961
Luke Ryan <hello@lukeryan.net.nz>
2015-06-07 20:24:31 +1200

    Fixing typo in requirements.txt

--- alfred/settings.py
+++ alfred/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'gunicorn',
     'alfredapi',
-    'alfredui'
+    'alfredui',
+    'corsheaders'
 )
 
 MIDDLEWARE_CLASSES = (
