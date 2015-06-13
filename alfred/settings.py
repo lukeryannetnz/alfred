26cf48a9bfe10dbc455c3a9dc1673e070dd79ac5
Luke Ryan <hello@lukeryan.net.nz>
2015-06-13 16:27:17 +1200

    make media within static so it is served by gunicorn

--- alfred/settings.py
+++ alfred/settings.py
@@ -107,6 +107,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
 CORS_ORIGIN_ALLOW_ALL = True
